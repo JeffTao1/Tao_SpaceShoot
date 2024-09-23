e91e0d769ab20b53f268e5ab7c678f74569d3eaf
JeffTao1 <jefft18ca@gmail.com>
2024-09-23 16:27:16 -0400

    task3 haven't done do it at school
    we

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    public float moveSpeed;
-    public float arrivalDistance;
-    public float maxFloatDistance;
-
+    public float moveSpeed = 2;
+    public float arrivalDistance = 1;
+    public float maxFloatDistance =10;
+    Vector3 whereTheirRandom;
     // Start is called before the first frame update
     void Start()
     {
+        AsteroidMovement();
     }
 
     // Update is called once per frame
     void Update()
     {
+        AsteroidMovement();
+    }
+    public void AsteroidMovement()
+    {
+        whereTheirRandom = transform.position + new Vector3( Random.Range(maxFloatDistance, -maxFloatDistance), Random.Range(-maxFloatDistance, -maxFloatDistance), 0);
+        transform.rotation = Random.rotation;
     }
 }
