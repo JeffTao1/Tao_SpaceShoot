8fed4f4b8aace2e7db82cc82c80b6d2b4c89505f
JeffTao1 <jefft18ca@gmail.com>
2024-09-23 20:30:53 -0400

    task4
    not done

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -10,5 +10,15 @@ public class Stars : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        DrawConstellation(starTransforms);
+    }
+    public void DrawConstellation(List<Transform> NextStar)
+    {
+        foreach (Transform starTransforms in NextStar)
+        {
+
+            Debug.DrawLine(transform.position, starTransforms.position, Color.green, drawingTime);
+
+        }
     }
 }
