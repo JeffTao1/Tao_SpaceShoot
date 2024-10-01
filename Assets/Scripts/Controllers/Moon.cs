8fde58137459f34fe42f9ee84bcbcbfde5d5fc43
JeffTao1 <jefft18ca@gmail.com>
2024-09-30 21:50:11 -0400

    Task1,2,3 done but not right

--- Assets/Scripts/Controllers/Moon.cs
+++ Assets/Scripts/Controllers/Moon.cs
@@ -1,11 +1,14 @@
+using Codice.CM.Common;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
 
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,18 @@ public class Moon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+       
+        OrbitalMotion(2,1,planetTransform);
     }
+    public void OrbitalMotion(float radius, float speed, Transform target)
+    {
+
+        float Angle = speed*Time.deltaTime*Mathf.Deg2Rad;
+
+        transform.position = new Vector3(Mathf.Cos(Angle) * radius, Mathf.Sin(Angle) * radius, Mathf.Tan(Angle) * radius) +target.position;
+       
+    }
+
+
+
 }
--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -1,4 +1,5 @@
 ﻿using Codice.CM.Common;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,8 +14,13 @@ public class Player : MonoBehaviour
     public float MaxSpeed = 10f;
     public float timeToMaxSpeed = 3f;
     float acceleration = 2f;
+    public GameObject powerUp;
 
     Vector3 velocity;
+    Color color;
+
+    public float radius = 5f;
+    public int points = 5;
 
     void Update()
     {
@@ -84,10 +90,10 @@ public class Player : MonoBehaviour
 
 
 
-
+        SpawnPowerups(3, 5);
 
         PlayerMovement();
-
+        EnemyRadar(radius, points);
     }
     public void PlayerMovement()
     {
@@ -96,10 +102,6 @@ public class Player : MonoBehaviour
 
         transform.position += velocity *  Time.deltaTime;
        
-        
-
-
-
         //if (Input.GetKey(KeyCode.UpArrow))
         //{
 
@@ -126,9 +128,40 @@ public class Player : MonoBehaviour
 
     }
 
-    public void EnemyRadar(float radius, int circlePoint)
+    public void EnemyRadar(float radius, int circlePoint=5)
     {
-    
+        Vector3 start = new Vector3(radius, 0, 0);
+        Vector3 next = start;
+        float whereIsEnemy = Vector3.Distance(transform.position,enemyTransform.position);
+        
+        for (int i = 0; i < circlePoint; i++)
+        {
+            float theAngle = 2f * Mathf.PI / (float)circlePoint * i;
+            Vector3 whereTheCircle = transform.position = new Vector3(Mathf.Cos(theAngle) * radius, Mathf.Sin(theAngle) * radius,0);
+            next = whereTheCircle;
+        }
+        if(whereIsEnemy<=radius)
+        {
+            color = Color.red;
+        }
+        else
+        {
+            color = Color.green;
+        }
+        
+        Debug.DrawLine(start,next,color);
     }
+    public void SpawnPowerups(float radius, int numberOfPowerups)
+    {
+        for(int i = 0;i < numberOfPowerups;i++)
+        {
+            float angle = 360f / numberOfPowerups;
+            Vector3 whereItSpawn = new Vector3(Mathf.Cos(angle) * radius, 0, Mathf.Sin(angle) * radius);
 
+            Instantiate(powerUp, whereItSpawn * Mathf.Deg2Rad, Quaternion.identity);
+        }
+
+
+       
+    }
 }
